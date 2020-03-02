50581d43686d3b920c5dc599c7f6e87381b355b2
Max Jahn <max.jahn@gmail.com>
2020-03-02 19:36:38 +0100

    updated sample environment to use Amsterdam region

--- infrastructure-setup/resources/set_env.sh
+++ infrastructure-setup/resources/set_env.sh
@@ -6,7 +6,7 @@ export TF_VAR_oci_fingerprint=xxxxxxxxxxxxxxx
 export TF_VAR_oci_private_key_path=.oci/oci_api_key.pem
 export TF_VAR_ssh_public_key=$(cat ~/.ssh/id_rsa.pub)
 
-export TF_VAR_oci_region=uk-london-1
+export TF_VAR_oci_region=eu-amsterdam-1
 
 # generate a password, e.g. using
 # apg -m 12 -n 1 -M SCNL 
@@ -19,21 +19,21 @@ export TF_VAR_oci_atp_db_workload="OLTP"
 export TF_VAR_oci_atp_db_cores="1"
 export TF_VAR_oci_atp_db_storage_tb="1"
 
-export TF_VAR_oci_azure_provider_ocid="ocid1.providerservice.oc1.uk-london-1.aaaaaaaa5kl2k7qdsraeezi6i6amkkjtdk543mcowvca2o7b3yqxkr27bxgq"
+export TF_VAR_oci_azure_provider_ocid="ocid1.providerservice.oc1.eu-amsterdam-1.aaaaaaaa63ndgdif5mke7gvq57hxmqhyg3cg2szzb3zseroieonof35fyajq"
 
 export TF_VAR_oci_service_vcn_cidr="10.0.0.0/16"
 export TF_VAR_oci_client_subnet_cidr="10.0.1.0/24"
 export TF_VAR_peering_net="10.99.0"
 
 # centos7 
-export TF_VAR_oci_base_image=ocid1.image.oc1.uk-london-1.aaaaaaaabf2eslezq4wejiu3kq7zbbbmkw5k55eltwmpgpgzju2t7q3nlx7q
+export TF_VAR_oci_base_image="ocid1.image.oc1.eu-amsterdam-1.aaaaaaaat32fvq5hsmbljrvy77gr2xel7i3l3oc6g3bcnnd6mimzz5jqa7ka"
 
 ## Azure
 export TF_VAR_arm_client_id="xxxxxxxxxxxxxx"
 export TF_VAR_arm_client_secret="xxxxxxxxxxxxxx"
 export TF_VAR_arm_tenant_id="xxxxxxxxxxxx"
 export TF_VAR_arm_subscription_id="xxxxxxxxxxxx"
-export TF_VAR_arm_region="UK South"
+export TF_VAR_arm_region="West Europe"
 
 export TF_VAR_arm_vnet_cidr="10.1.0.0/16"
 export TF_VAR_arm_client_subnet_cidr="10.1.1.0/24"
