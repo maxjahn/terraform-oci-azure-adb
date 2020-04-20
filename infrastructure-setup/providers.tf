10d315ca33034b97c9fcfc75162477e26e9470a2
maxjahn <max.jahn@gmail.com>
2020-04-20 12:16:01 +0200

    updated azure provider

--- infrastructure-setup/providers.tf
+++ infrastructure-setup/providers.tf
@@ -13,6 +13,7 @@ provider "azurerm" {
   client_id       = "${var.arm_client_id}"
   client_secret   = "${var.arm_client_secret}"
   tenant_id       = "${var.arm_tenant_id}"
+  features {}
 }
 
 provider "local" {
